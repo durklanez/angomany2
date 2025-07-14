1ba36d3f4b77a55279cd01c5a936b82b14f1b8b1
durklanez <durkjosue@gmail.com>
2025-07-14 17:22:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("✅ Bot funcionando! Agora o Render encontrou o main.py.")
+import os
+import telebot
+from telebot import types
+import firebase_admin
+from firebase_admin import credentials, db
+from keep_alive import keep_alive
+
+# Token do bot
+TOKEN = os.environ.get("7474368528:AAF1zyWlVuIpoqyQtFhZV7mvVQrHuo_WmOE")
+bot = telebot.TeleBot(TOKEN)
+
+# Inicializar Firebase
+cred = credentials.Certificate("firebase-credentials.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://angocas-8b3e3-default-rtdb.firebaseio.com/
+})
+
+def get_user_ref(user_id):
+    return db.reference(f"users/{user_id}")
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    uid = msg.from_user.id
+    ref = get_user_ref(uid)
+    if not ref.get():
+        ref.set({
+            'saldo_ango': 0.0,
+            'saldo_dolar': 0.0
+        })
+    bot.reply_to(msg, "🎰 Bem-vindo ao Cassino ANGO!")
+
+# Ativa webserver do keep_alive
+keep_alive()
+
+# Inicia o bot
+bot.polling(non_stop=True)
+
